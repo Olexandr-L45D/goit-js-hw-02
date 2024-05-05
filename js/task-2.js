6ed120ea89a38073ded0b08dd09cfdd966bafa9a
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-05-05 18:29:37 +0300

    task 2 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,6 @@
-// 2
+
 function formatMessage(messages, maxLength) {
        return messages.slice(0, maxLength) + "...";
     }
     
-    console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-    console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+    
